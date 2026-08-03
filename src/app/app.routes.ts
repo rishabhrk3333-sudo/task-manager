ad872476bba87b623a00d31a27d301e17c7c66d4
Rishabh <rishabh@macbook.local>
2026-08-03 22:49:29 +0530

    routing is added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '',
+        redirectTo: 'tasks',
+        pathMatch:'full'
+    },
+    {
+        path:'tasks',
+        loadComponent: () => 
+            import ('./pages/task-list/task-list').then(
+                (m)=> m.TaskList
+        )
+    },
+     {
+    path: 'tasks/new',
+    loadComponent: () =>
+      import('./pages/task-form/task-form').then(
+        (m) => m.TaskForm
+      ),
+  },
+  {
+    path: 'tasks/:id/edit',
+    loadComponent: () =>
+      import('./pages/task-form/task-form').then(
+        (m) => m.TaskForm
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/not-found/not-found').then(
+        (m) => m.NotFound
+      ),
+  },
+];
